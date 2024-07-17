808a9fd45c39484fa10534a1123fc504edf7fd68
Shalpalani <shalinipalanisamy1712@gmail.com>
2024-07-17 14:15:46 +0530

    Implement signIn and signUp

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -1,17 +1,16 @@
 'use client';
 import React from 'react'
-
 import CountUp from 'react-countup';
 const AnimatedCounter = ({amount}:{amount:number}) => {
   return (
     <div className='w-full'>
-         <CountUp 
-         decimals={2}
+        <CountUp end={amount} 
+        duration={2.75}
+        decimals={2}
          decimal=','
-         prefix='$'
-         end={amount} />
+       prefix='$'
+       />
     </div>
-   
   )
 }
 
